f82bd5230b17e30d0cfc0239f5e23acfb66f9156
sabrina topel <Srayza26@academicos.com>
2024-06-29 13:16:17 -0300

    feat: form de criar conta

--- src/pages/CriarConta/index.tsx
+++ src/pages/CriarConta/index.tsx
@@ -1,13 +1,13 @@
 import { useState, ChangeEvent } from "react";
 import { Card, TextField, Button } from "@mui/material";
 import majorLogo from '../../assets/img/logo-major.png'
-import { Link } from "react-router-dom";
-const Login = () => {
-  const [usuario, setUsuario] = useState<any>({ cpf: null, senha: null });
+
+const Cadastro = () => {
+    const [usuario, setUsuario] = useState<any>({ nome: null,cpf: null, senha: null });
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
-    setUsuario((prevLogin: any) => ({
+    setUsuario((prevLogin:any) => ({
       ...prevLogin,
       [id]: value
     }));
@@ -19,20 +19,16 @@ const Login = () => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} >
-      <Card sx={{ width: '30%', textAlign: 'center', padding: '20px', display: 'flex', flexDirection: 'column' }} >
-        <img src={majorLogo} alt="Major Logo" width={200} style={{ margin: 'auto' }} />
+      <Card sx={{ width: '30%', textAlign: 'center', padding: '20px', display:'flex', flexDirection:'column' }} >
+      <img src={majorLogo} alt="Major Logo" width={200}  style={{margin:'auto'}}/>
+      <TextField id="nome" label="Nome" variant="outlined" onChange={handleInputChange} value={usuario.nome || ''} style={{ marginBottom: '20px' }} />
         <TextField id="cpf" label="CPF" variant="outlined" onChange={handleInputChange} value={usuario.cpf || ''} style={{ marginBottom: '20px' }} />
         <TextField id="senha" label="Senha" variant="outlined" type="password" onChange={handleInputChange} value={usuario.senha || ''} style={{ marginBottom: '20px' }} />
-        <Button id='entrar' variant="contained" color="primary" onClick={handleSubmit}>Entrar</Button>
-        <hr style={{ marginTop: '10px', marginBottom: '10px' }} />
-
-
-        <Link to="/criar-conta" style={{ textDecoration: 'none', width: '100%' }}>
-          <Button variant="contained" id='criarConta' style={{ backgroundColor: '#002884', width: '100%' }} onClick={handleSubmit}>Criar uma conta</Button>
-        </Link>
+       
+        <Button variant="contained"   style={{backgroundColor:'#002884'}} onClick={handleSubmit}>Criar Conta</Button>
       </Card>
     </div>
   );
 };
 
-export default Login;
+export default Cadastro;
