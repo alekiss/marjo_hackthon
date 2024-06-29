8b3d7168804ac44bbcd594e385f66ddfc046d080
sabrina topel <Srayza26@academicos.com>
2024-06-29 15:49:27 -0300

    feat: header na nova doacao

--- src/pages/NovaDoacao/index.tsx
+++ src/pages/NovaDoacao/index.tsx
@@ -12,6 +12,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Header from '../../components/Header';
 
 
 const steps = ['Preencher Dados', 'Confirmar Dados'];
@@ -92,100 +93,111 @@ export default function HorizontalLinearStepper() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Card>
 
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const stepProps: { completed?: boolean } = {};
-            const labelProps: {
-              optional?: React.ReactNode;
-            } = {};
+      <Box display="flex" flexDirection="column" pt="spacing-16" px="spacing-12">
+        <Header />
 
-            if (isStepSkipped(index)) {
-              stepProps.completed = false;
-            }
-            return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-              </Step>
-            );
-          })}
-        </Stepper>
-        {activeStep === steps.length ? (
-          <React.Fragment>
-            <div style={{ height: '50vh' }}>
-              <Typography sx={{ mt: 2, mb: 1, textAlign: 'center' }}>
-                <h3>   Transação realizada com sucesso! <br /> Obrigada, sua ajuda faz a diferença!</h3>
-              </Typography>
 
-              <div style={{ width: '100%', justifyContent: 'center', display: 'flex', gap: '5px', marginTop: '20px' }}>
-                <Button variant="contained" disabled>Baixar Comprovante</Button>
-                <div>
-                  <Link to="/" style={{ textDecoration: 'none', width: '100%' }}>
-                    <Button variant="contained" onClick={handleReset}>Ir para Transações </Button>
+        <Card >
 
-                  </Link>
+          <div style={{ height: '75vh', marginTop: '20px', padding: 20 }}>
+          <h2>Nova Doação</h2> <br />
+            <Stepper activeStep={activeStep}>
+              {steps.map((label, index) => {
+                const stepProps: { completed?: boolean } = {};
+                const labelProps: {
+                  optional?: React.ReactNode;
+                } = {};
+
+                if (isStepSkipped(index)) {
+                  stepProps.completed = false;
+                }
+                return (
+                  <Step key={label} {...stepProps}>
+                    <StepLabel {...labelProps}>{label}</StepLabel>
+                  </Step>
+                );
+              })}
+            </Stepper>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <div style={{ height: '50vh' }}>
+                  <Typography sx={{ mt: 2, mb: 1, textAlign: 'center' }}>
+                    <h3>   Transação realizada com sucesso! <br /> Obrigada, sua ajuda faz a diferença!</h3>
+                  </Typography>
+
+                  <div style={{ width: '100%', justifyContent: 'center', display: 'flex', gap: '5px', marginTop: '20px' }}>
+                    <Button variant="contained" disabled>Baixar Comprovante</Button>
+                    <div>
+                      <Link to="/" style={{ textDecoration: 'none', width: '100%' }}>
+                        <Button variant="contained" onClick={handleReset}>Ir para Transações </Button>
+
+                      </Link>
+                    </div>
+
+                  </div>
                 </div>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
 
-              </div>
-            </div>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
+                {activeStep == 0 && (<>
+                  <div style={{ width: '30%', textAlign: 'justify', padding: '20px', display: 'flex', gap: '5%', justifyContent: 'center' }} >
 
-            {activeStep == 0 && (<>
-              <div style={{ width: '30%', textAlign: 'justify', padding: '20px', display: 'flex', gap: '5%', justifyContent: 'center' }} >
+                    <TextField id="descricao" label="Descrição" variant="outlined" onChange={handleInputChange} value={doacao.descricao || ''} style={{ marginBottom: '20px' }} />
+                    <TextField id="valor" label="Valor*" variant="outlined" type="text" onChange={handleInputChange} value={doacao.valor || ''} style={{ marginBottom: '20px' }} />
 
-                <TextField id="descricao" label="Descrição" variant="outlined" onChange={handleInputChange} value={doacao.descricao || ''} style={{ marginBottom: '20px' }} />
-                <TextField id="valor" label="Valor*" variant="outlined" type="text" onChange={handleInputChange} value={doacao.valor || ''} style={{ marginBottom: '20px' }} />
-
-              </div>
+                  </div>
 
 
-            </>)}
-            {activeStep == 1 && (<>
-              <div style={{ padding: '20px', display: 'flex', gap: '5%', justifyContent: 'center' }} >
-                <h3>Descrição: {doacao.descricao ? doacao.descrica : 'Não Informado'}</h3>
-                <h3>Valor: {doacao.valor}</h3>
+                </>)}
+                {activeStep == 1 && (<>
+                  <div style={{ padding: '20px', display: 'flex', gap: '5%', justifyContent: 'center' }} >
+                    <h3>Descrição: {doacao.descricao ? doacao.descrica : 'Não Informado'}</h3>
+                    <h3>Valor: {doacao.valor}</h3>
 
-              </div>
-            </>)}
+                  </div>
+                </>)}
 
-            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-              {activeStep == 0 ? <>
-                <Button
-                  color="inherit"
-                  hidden={activeStep === 0}
-                  onClick={handleSair}
-                  sx={{ mr: 1 }}
-                >
-                  Voltar
-                </Button></> : <>
-                <Button
-                  color="inherit"
-                  hidden={activeStep === 0}
-                  onClick={handleBack}
-                  sx={{ mr: 1 }}
-                >
-                  Voltar
-                </Button></>}
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                  {activeStep == 0 ? <>
+                    <Button
+                      color="inherit"
+                      hidden={activeStep === 0}
+                      onClick={handleSair}
+                      sx={{ mr: 1 }}
+                    >
+                      Voltar
+                    </Button></> : <>
+                    <Button
+                      color="inherit"
+                      hidden={activeStep === 0}
+                      onClick={handleBack}
+                      sx={{ mr: 1 }}
+                    >
+                      Voltar
+                    </Button></>}
 
-              <Box sx={{ flex: '1 1 auto' }} />
+                  <Box sx={{ flex: '1 1 auto' }} />
 
-              {activeStep === steps.length - 1 ? <><Button onClick={handleClickOpen}>
-                Confirmar
+                  {activeStep === steps.length - 1 ? <><Button onClick={handleClickOpen}>
+                    Confirmar
 
-              </Button></> : <>
-                <Button onClick={handleNext}>
-                  Próximo
+                  </Button></> : <>
+                    <Button onClick={handleNext}>
+                      Próximo
 
-                </Button></>}
+                    </Button></>}
 
-            </Box>
+                </Box>
 
-          </React.Fragment>
-        )}
-      </Card>
+              </React.Fragment>
+            )}
+          </div>
+
+
+        </Card>
+      </Box>
       <Snackbar
         open={open}
         autoHideDuration={6000}
