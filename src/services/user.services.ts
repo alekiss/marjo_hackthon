60ce4549c8dd9655516a27cb49bf42d4aaaab375
sabrina topel <Srayza26@academicos.com>
2024-06-29 16:52:16 -0300

    fix: api

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,24 +1,24 @@
-import axios, { AxiosResponse } from 'axios';
+import api from "./api";
 
 
 export const logar = async (body:any): Promise<AxiosResponse<any>> => {
-    const response = await axios.post('/api/token', body)
+    const response = await api.post('/api/token', body)
     return response;
 };
 
 export const createUsuario = async (body: any): Promise<AxiosResponse<any>> => {
-    const response = await axios.post('/create-user', body)
+    const response = await api.post('/create-user', body)
     return response;
 };
 
 
 export const getTransacaoListByIdUsuario = async (id: any): Promise<AxiosResponse<any>> => {
-      const response = await axios.get('/transacao/'+id);
+      const response = await api.get('/transacao/'+id);
     return response;
 
 };
 
 export const createNovaDoacao = async (body: any): Promise<AxiosResponse<any>> => {
-      const response = await axios.post('/doacoes', body);
+      const response = await api.post('/doacoes', body);
     return response;
 };
