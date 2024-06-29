04ec1250a1681a58b59da388dedba0062746f06f
sabrina topel <Srayza26@academicos.com>
2024-06-29 14:41:15 -0300

    feat: step1 e step finalizada

--- src/pages/NovaDoacao/index.tsx
+++ src/pages/NovaDoacao/index.tsx
@@ -1,12 +1,22 @@
-import React, { useState, ChangeEvent } from "react";
-import { Card, TextField, Button } from "@mui/material";
-import { useNavigate } from "react-router";
-import Snackbar from '@mui/material/Snackbar';
-const Login = () => {
-  const [doacao, setDoacao] = useState<any>({ valor: null, descricao: null });
-  const [mensagemErro, setMensagemErro] = useState<any>("");
-  const navigate = useNavigate()
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Card, Snackbar, TextField } from '@mui/material';
+
+const steps = ['Preencher Dados', 'Confirmar Dados'];
+
+export default function HorizontalLinearStepper() {
+  const [doacao, setDoacao] = React.useState<any>({ valor: null, descricao: null });
+  const [open, setOpen] = React.useState(false)
+  const [mensagemErro, setMensagemErro] = React.useState<any>("");
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set<number>());
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
     setDoacao((prevLogin: any) => ({
       ...prevLogin,
@@ -14,41 +24,99 @@ const Login = () => {
     }));
   };
 
-  const [open, setOpen] = useState(false);
-  const handleProximo = () => {
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
 
-    if (doacao.valor == null || doacao.valor.length == 0) {
-      setMensagemErro('Digite um valor!')
-      setOpen(true);
-      return
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
     }
 
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
 
-
-
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
 
-  const handleVoltar = () => {
-   console.log('voltei')
-    navigate('/')
-
-
-
+  const handleReset = () => {
+    setActiveStep(0);
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} >
-      <Card sx={{ width: '30%', textAlign: 'justify', padding: '20px', display: 'flex', flexDirection: 'column' }} >
-        <h2>Preencha os Dados</h2> <br />
-        <TextField id="descricao" label="Descrição" variant="outlined" onChange={handleInputChange} value={doacao.descricao || ''} style={{ marginBottom: '20px' }} />
-        <TextField id="valor" label="Valor" variant="outlined" type="text" onChange={handleInputChange} value={doacao.valor || ''} style={{ marginBottom: '20px' }} />
-     
-        <div style={{ display: 'flex' }}>
-        <Button id='voltar' variant="contained" style={{ width: '100%',margin:10 }} color="primary" onClick={handleVoltar}>Voltar</Button>
-          <Button variant="contained" id='proximo' style={{ backgroundColor: '#002884', width: '100%',margin:10 }} onClick={handleProximo}>Próximo</Button>
-        </div>
-        </Card>
+    <Box sx={{ width: '100%' }}>
+      <Card>
+
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps: { completed?: boolean } = {};
+            const labelProps: {
+              optional?: React.ReactNode;
+            } = {};
+
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <div style={{ height: '50vh' }}>
+              <Typography sx={{ mt: 2, mb: 1, textAlign: 'center' }}>
+                <h3>   Transação realizada com sucesso! <br /> Obrigada, sua ajuda faz a diferença!</h3>
+
+              </Typography>
+
+              <div style={{ width: '100%', justifyContent: 'center', display: 'flex', gap: '5px', marginTop: '20px' }}>
+                <Button variant="contained" disabled>Baixar Comprovante</Button>
+                <Button variant="contained" onClick={handleReset}>Ir para Home! </Button>
+              </div>
+            </div>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+
+            {activeStep == 0 && (<>
+              <div style={{ width: '30%', textAlign: 'justify', padding: '20px', display: 'flex', gap: '5%', justifyContent: 'center' }} >
+
+                <TextField id="descricao" label="Descrição" variant="outlined" onChange={handleInputChange} value={doacao.descricao || ''} style={{ marginBottom: '20px' }} />
+                <TextField id="valor" label="Valor" variant="outlined" type="text" onChange={handleInputChange} value={doacao.valor || ''} style={{ marginBottom: '20px' }} />
+
+              </div>
+
+
+            </>)}
+            {activeStep == 1 && (<>oi2</>)}
+            {activeStep == 2 && (<>oi3</>)}
+
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                hidden={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Voltar
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Confirmar' : 'Próximo'}
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </Card>
       <Snackbar
         open={open}
         autoHideDuration={6000}
@@ -56,8 +124,6 @@ const Login = () => {
 
 
       />
-    </div>
+    </Box>
   );
-};
-
-export default Login;
+}
