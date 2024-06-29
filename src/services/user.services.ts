f1911fedea74d0be311363cf0ef894215efefd83
Ramon Aires <ramon@MacBook-Air-de-Ramon.local>
2024-06-29 16:32:33 -0300

    fix

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,24 +1,24 @@
 import axios, { AxiosResponse } from 'axios';
 
 const api = axios.create({
-    baseURL: 'localhost:8080',
+    baseURL: 'http://127.0.0.1:8000',
 });
 
 export const logar = async (): Promise<AxiosResponse<any>> => {
-    const response = api.post('/login')
+    const response = await api.post('/login')
     console.log(response)
-    return response;
+    return response.data;
 };
 
 export const createUsuario = async (body: any): Promise<AxiosResponse<any>> => {
-    const response = api.post('/create-user', body)
+    const response = api.post('/create-user/', body)
     console.log(response)
     return response;
 };
 
 
 export const getTransacaoListByIdUsuario = async (id: any): Promise<AxiosResponse<any>> => {
-      const response = api.get('/transacao/'+id);
+    const response = api.get('/transacao/'+id);
     console.log(response)
     return response;
 
