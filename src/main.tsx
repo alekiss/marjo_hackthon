b2b2594bf0f93696a890f9f0a3dfa66c908cbdc4
alekiss <alekiss.melo@gmail.com>
2024-06-29 12:23:23 -0300

    feat: Organização das pastas e instalação de bibliotecas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/index.tsx";
+import Home from "./pages/Home/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@mui/material"
+
+const Login = () => {
+  return (
+    <Card sx={{maxWidth: 575, minHeight: 400}}>
+      TESTE
+    </Card>
+  )
+}
+
+export default Login
