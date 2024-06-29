9f2a69253cfa60e255d50df3e67ed977d507313c
sabrina topel <Srayza26@academicos.com>
2024-06-29 16:18:01 -0300

    feat: services api

--- src/pages/CriarConta/index.tsx
+++ src/pages/CriarConta/index.tsx
@@ -1,8 +1,9 @@
 import { useState, ChangeEvent } from "react";
 import { Card, TextField, Button } from "@mui/material";
-import majorLogo from '../../assets/img/logo-major.png'
 import { cpf } from 'cpf-cnpj-validator';
 import Snackbar from '@mui/material/Snackbar';
+
+import { createUsuario } from "../../services/user.services";
 
 const Cadastro = () => {
   const [usuario, setUsuario] = useState<any>({ nome: null, cpf: null, senha: null });
@@ -43,17 +44,29 @@ const Cadastro = () => {
     } else {
       console.log('entrar')
       setOpen(false)
+      criarUsuario()
     }
 
   };
+
+
+  const criarUsuario = () => {
+    createUsuario(usuario).then((res)=>{
+      console.log(res)
+    })
+  }
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }} >
       <Card sx={{ width: '30%', textAlign: 'center', padding: '20px', display: 'flex', flexDirection: 'column' }} >
-        <img src={majorLogo} alt="Major Logo" width={200} style={{ margin: 'auto' }} />
+        <img src={'./logo-major.png'} alt="Major Logo" width={200} style={{ margin: 'auto' }} />
+        <TextField id="username" label="Usuário" variant="outlined" onChange={handleInputChange} value={usuario.username || ''} style={{ marginBottom: '20px' }} />
+       <TextField id="email" label="E-Mail" variant="outlined" onChange={handleInputChange} value={usuario.email || ''} style={{ marginBottom: '20px' }} />
+     
         <TextField id="nome" label="Nome*" variant="outlined" onChange={handleInputChange} value={usuario.nome || ''} style={{ marginBottom: '20px' }} />
         <TextField id="cpf" label="CPF*" variant="outlined" onChange={handleInputChange} value={usuario.cpf || ''} style={{ marginBottom: '20px' }} />
-        <TextField id="senha" label="Senha*" variant="outlined" type="password" onChange={handleInputChange} value={usuario.senha || ''} style={{ marginBottom: '20px' }} />
-
+        <TextField id="password" label="Senha" variant="outlined" type="password" onChange={handleInputChange} value={usuario.password || ''} style={{ marginBottom: '20px' }} />
+      
         <Button variant="contained" style={{ backgroundColor: '#002884' }} onClick={handleSubmit}>Criar Conta</Button>
       </Card>
       <Snackbar
