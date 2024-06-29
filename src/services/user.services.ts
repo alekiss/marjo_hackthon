3558f5c72c14ea3f14c5ef1fd916e5a453c13ec4
sabrina topel <Srayza26@academicos.com>
2024-06-29 17:54:09 -0300

    refact: tirando codigo antigo

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -9,32 +9,20 @@ export const logar = async (body: any): Promise<any> => {
 
 export const createUsuario = async (body: any): Promise<any> => {
     const response = await api.post('/create-user', body)
-    if (response.status == 401) {
-        navigate('/login')
-        localStorage.clear()
-        return;
-    }
+  
     return response;
 };
 
 
 export const getTransacaoList = async (): Promise<any> => {
     const response = await api.get('doacao/minhas-doacoes/');
-    if (response.status == 401) {
-        navigate('/login')
-        localStorage.clear()
-        return;
-    }
+   
     return response;
 
 };
 
 export const createNovaDoacao = async (body: any): Promise<any> => {
     const response = await api.post('doacao/doacoes/', body);
-    if (response.status == 401) {
-        navigate('/login')
-        localStorage.clear()
-        return;
-    }
+   
     return response;
 };
